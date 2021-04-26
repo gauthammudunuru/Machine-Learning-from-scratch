bca637caaaa64d328d226983d52115ac5ef984c9
Gautham Viswanadha Varma Mudunuru <32221326+mgauthamvarma@users.noreply.github.com>
2021-04-26 14:03:30 -0500

    Update logistic.m

--- logistic.m
+++ logistic.m
@@ -1,6 +1,4 @@
 function [w] = logistic(A,t)
-    % A = data(known)
-    % t = target(known)
     iter = 100000;
     [N,d] = size(A);
     o = ones(N,1);
@@ -8,19 +6,13 @@ function [w] = logistic(A,t)
     w = rand(d+1,1);
     R =1e-3;
     J = (sigmoid_activate(x(1,:)*w)-t(1))*x(1,:);
-    
     for i =1:iter
-        
         for j = 1:N
             J = J + (sigmoid_activate(x(j,:)*w)-t(j))*x(j,:);
         end
-        
         w = w-R*J';
-        
         if(J <= 0)
             break;
         end  
-        
     end
-
 end
